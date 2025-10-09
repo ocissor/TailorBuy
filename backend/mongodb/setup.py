a0f11a79d915306341eabf236940ae609df68120
ocissor <sarthakbatra100@gmail.com>
2025-10-09 15:42:37 +0100

    updated frontend service to NodePort

--- backend/mongodb/setup.py
+++ backend/mongodb/setup.py
@@ -12,7 +12,7 @@ password = urllib.parse.quote(password)
 uri = f"{p1}{password}{p2}"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, tlsAllowInvalidCertificates=True)
+client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
 try:
